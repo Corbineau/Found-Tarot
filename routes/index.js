bfef51c258d7092ce436ec006391ea5c3b20485d
corbineau <lochinvar@gmail.com>
2022-08-29 01:37:32 -0400

    rate limiter stuff #2

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,23 @@ const apiRoutes = require("./api");
 // API Routes
 router.use("/api", apiRoutes);
 
-// If no API routes are hit, send the React app
-router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "../client/public/index.html"));
+// set up rate limiter: maximum of five requests per minute
+const RateLimit = require('express-rate-limit');
+var limiter = new RateLimit({
+  windowMs: 1*60*1000, // 1 minute
+  max: 5
 });
+
+// apply rate limiter to all requests
+app.use(limiter);
+
+// If no API routes are hit, send the React app
+app.get('/:path', function(req, res) {
+  let path = req.params.path;
+  if (isValidPath(path))
+    res.sendFile(path.join(__dirname, "../client/public/index.html"));
+});
+
+
 
 module.exports = router;
