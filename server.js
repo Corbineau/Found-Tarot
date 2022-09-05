811309c64292320c5aca609116c3f9ac89ae85ff
corbineau <lochinvar@gmail.com>
2022-09-05 19:59:21 -0400

    added sandbox database connection

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
+require('dotenv').config();
 const express = require("express");
 const path = require("path");
 const routes = require("../Found-Tarot/routes");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const un = process.env.USERNAME
+const password = process.env.PASSWORD 
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 
 mongoose.connect(
-  "mongooseurl",
+  `mongodb+srv://${un}:${password}@clustersandbox0.3wlssml.mongodb.net/?retryWrites=true&w=majority`,
   {
     useNewUrlParser: true
   }
